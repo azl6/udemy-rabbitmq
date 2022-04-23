b843ca53e7893f8ba9a6c404dc026165fea762cd
Alex <alex.rodrigues23997@gmail.com>
2022-04-22 21:45:22 -0300

    Configurando rejeição manual de mensagem

--- rabbitmq.consumer/src/main/java/com/course/rabbitmq/consumer/consumer/MyPictureImageConsumer.java
+++ rabbitmq.consumer/src/main/java/com/course/rabbitmq/consumer/consumer/MyPictureImageConsumer.java
@@ -3,12 +3,17 @@ package com.course.rabbitmq.consumer.consumer;//package com.course.rabbitmq.cons
 import com.course.rabbitmq.consumer.entity.Picture;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class MyPictureImageConsumer {
@@ -19,11 +24,14 @@ public class MyPictureImageConsumer {
     private static final Logger LOG = LoggerFactory.getLogger(MyPictureImageConsumer.class);
 
     @RabbitListener(queues = "q.mypicture.image")
-    public void listen(String message) throws JsonProcessingException {
+    public void listen(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
         var picture = objectMapper.readValue(message, Picture.class);
 
-        if(Integer.valueOf(picture.getSize()) > 9000)
-            throw new AmqpRejectAndDontRequeueException("Picture's size is too large");
+        if(Integer.valueOf(picture.getSize()) > 9000){
+            channel.basicReject(tag, false);
+//            throw new AmqpRejectAndDontRequeueException("Picture's size is too large");
+        }
         LOG.info("On image: {}", picture);
+        channel.basicAck(tag, false);
     }
 }
